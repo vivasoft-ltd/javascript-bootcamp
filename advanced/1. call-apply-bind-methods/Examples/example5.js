1e981f7da79658aa2f9f122777de4c8ee84ad86c
Jaber Al Nahian <jaber@Jabers-MacBook-Pro.local>
2022-01-12 16:40:34 +0600

    Adding first example of call, apply and bind methods

---
+++ advanced/1. call-apply-bind-methods/Examples/example5.js
@@ -0,0 +1,65 @@
+//Examples for Call(), Apply() and Bind() Methods
+//Live Example: https://jsfiddle.net/rijans/6tnejcLa/7/
+
+//.call() Method Example:
+
+//1st Example of .call() method
+let bike = {
+    name: 'Suzuki Gixxer SF'
+}
+
+let getBikeInfo = function (ccValue) {
+    return this.name + " is " + ccValue + " CC.";
+}
+
+console.log(getBikeInfo.call(bike, 150));
+//Output: Suzuki Gixxer SF is 150 CC.
+
+//2nd Example of .call method
+function MotorBike(wheelCount, engineCount){
+    this.wheels = wheelCount;
+    this.engines = engineCount;
+}
+
+function GixxerSF(wheelCount, engineCount, engineCC, brand){
+    MotorBike.call(this, wheelCount, engineCount);
+    this.engineCC = engineCC;
+    this.brand = brand;
+}
+
+let newGixxerSF = new GixxerSF(2,1, 150, 'Suzuki');
+console.log(newGixxerSF);
+//Output:
+// {
+//     brand: "Suzuki",
+//     engineCC: 150,
+//     engines: 1,
+//     wheels: 2
+// }
+
+
+
+//.apply() Method Example:
+function bicycle(wheelCount, engineCount){
+    this.wheels = wheelCount;
+    this.engines = engineCount;
+}
+
+function bicycleModelX(wheelCount, engineCount, brand){
+    MotorBike.apply(this, arguments);
+    this.brand = brand;
+}
+
+let newBicycleModelX = new bicycleModelX(2,0, 'Phonix');
+console.log(newBicycleModelX);
+//Output::
+//{
+//    brand: "Phonix",
+//    engines: 0,
+//    wheels: 2
+//}
+
+
+
+//.bind() Method Example
+
